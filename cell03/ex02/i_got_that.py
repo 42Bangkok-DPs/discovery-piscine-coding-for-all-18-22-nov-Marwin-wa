a03be2a1266f16f533723cbbdcae78f241cc648f
Marwin-wa <67160055@kmitl.ac.th>
2024-11-20 16:30:39 +0000

    i_got_that

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,12 @@
+
+while True:
+    
+    user_input = input("What you gotta say? : ")
+    
+
+    if user_input == "STOP":
+        print("I got that!")
+        break
+    
+    # Respond to the user
+    print("I got that! Anything else?")
