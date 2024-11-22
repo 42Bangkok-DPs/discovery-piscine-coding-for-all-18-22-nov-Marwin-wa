e62fe148d88c5bfedaf2379fc1bab0f104b1bda4
Marwin-wa <67160055@kmitl.ac.th>
2024-11-22 04:02:38 +0000

    asdfsdafsadf

--- Rush/checkmate.py
+++ Rush/checkmate.py
@@ -3,7 +3,7 @@ def checkmate(board):
     board = board.strip().split('\n')
     size = len(board)
     
-        king_pos = None
+    king_pos = None
     for i in range(size):
         for j in range(size):
             if board[i][j] == 'K':
